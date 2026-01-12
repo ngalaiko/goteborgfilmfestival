a4127e10a23f2340d11fccad6748af4bfa16fb3b
Nikita Galaiko <nikita@galaiko.rocks>
2026-01-12 22:49:34 +0100

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { injectAnalytics } from '@vercel/analytics/sveltekit'
+  import { dev } from '$app/environment';
   import "../app.css";
 
   let { children } = $props();
+  
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
